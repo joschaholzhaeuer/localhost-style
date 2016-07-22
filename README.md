6be9e915ff82042a4af84f4b0c6d0744ea5b2b31
Joscha Holzhäuer <j.holzhaeuer@outlook.com>
2016-07-22 14:23:29 +0200

    Added files and README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# localhost-style
+# Localhost Style
+
 A custom index.php and CSS styles for your localhost.
+
+## How to Apply
+
+* Copy `index.php` in the root directory of localhost (e.g. `MAMP/sites`)
+* Create a folder for each category (e.g. `MAMP/sites/workspace` and `MAMP/sites/personal`)
+* Rename `index-workspace.php` to `index.php` and copy it into once into every folder you created
+* Open each `index.php` and apply the `active`-class to the current folder/category
---
+++ index-workspace.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Localhost</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="../index-custom-style.css">
+    </head>
+    <body>
+
+        <div class="wrapper">
+
+            <!-- <h1 class="heading">Localhost</h1> -->
+
+            <div class="projekte">
+                <nav class="projekte-nav">
+                    <a href="http://localhost/dreiqbik/" class="projekte-link">DreiQbik</a>
+                    <a href="http://localhost/workspace/" class="projekte-link active">Workspace</a>
+                    <a href="http://localhost/phpMyAdmin/" class="link-ext"><em>php</em>MyAdmin</a>
+                </nav>
+            </div>
+
+            <div class="wrapper-inner">
+                <ul class="list">
+
+                   <?php
+
+                   function scan_dir($dir_lm) {
+                       $ignored = array('.', '..', '.svn', '.htaccess');
+
+                       $files = array();
+                       foreach (scandir($dir_lm) as $file) {
+                           if (in_array($file, $ignored)) continue;
+                           $files[$file] = filemtime($dir_lm . '/' . $file);
+                       }
+
+                       arsort($files);
+                       $files = array_keys($files);
+
+                       return ($files) ? $files : false;
+                   }
+
+                   $dir = scan_dir(getcwd());
+
+                   ?>
+
+                   <?php foreach ($dir as $file) : ?>
+
+                       <?php if( $file != "." && $file != ".." && $file != "index.php"  ) : ?>
+
+                        <li class="list-item">
+                            <a class="link" href="<?php echo $file; ?>"><?php echo $file; ?></a>
+                            <a class="link-btn" href="http://<?php echo $file; ?>"><em>v</em>Host</a>
+                        </li>
+
+                       <?php endif; ?>
+
+                   <?php endforeach; ?>
+
+                </ul>
+
+                <div class="sites">
+                    <h2 class="sites-title">Links</h2>
+                    <ul class="sites-items">
+                        <li class="sites-item">
+                            <a href="https://github.com" class="sites-link">GitHub<br><span class="sites-link-desc">Homepage</span></a>
+                        </li>
+                        <li class="sites-item">
+                            <a href="https://trello.com/login" class="sites-link">Trello<br><span class="sites-link-desc">Login</span></a>
+                        </li>
+                        <li class="sites-item">
+                            <a href="https://kas.all-inkl.com/" class="sites-link">ALL-INKL<br><span class="sites-link-desc">KAS-Server</span></a>
+                        </li>
+                        <li class="sites-item">
+                            <a href="https://developer.wordpress.org/reference/" class="sites-link">Wordpress<br><span class="sites-link-desc">Codex</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
